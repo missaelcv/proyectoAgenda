8e8f11d6d7fae8181f7d06569b719674717a1ee8
missaelcv <missaelcv0220@gmail.com>
2021-09-22 00:09:21 -0400

    Agregando Variables

--- ProyectoAgenda/Contacto.cs
+++ ProyectoAgenda/Contacto.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace ProyectoAgenda
 {
-    class Contacto
+    public class Contacto
     {
+        public string Nombre { get; set;  }
+
+        public string Apellido { get; set; }
+
+        public int TelefonoMovil { get; set; }
+
+        public int TelefonoCasa { get; set; }
+
+        public string CorreoElectronico { get; set; }
+
+        public string Direccion { get; set; }
+        public string Cuidades { get; set; }
+        
+
     }
 }
